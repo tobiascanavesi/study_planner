ca31dea97e4df30f16a8053e60629b6496d66424
Tobias <63179531+tobiascanavesi@users.noreply.github.com>
2024-05-27 18:47:32 -0300

    Updating planner

--- study_planner.py
+++ study_planner.py
@@ -1,72 +1,43 @@
-import os
-from crewai import Task, Crew, Process
-from crewai_tools import FileReadTool, SerperDevTool, ScrapeWebsiteTool
+from crewai import Crew, Process
+from langchain_openai import ChatOpenAI
+from tasks import Tasks
 from crewai_components import Agents
+
+import agentops
 
 class StudyPlanner:
     def __init__(self):
+        agentops.init(tags=['study_planner'],max_wait_time=3)
+        self.tasks = Tasks()
         self.agents = Agents()
+        self.study_plan_crew = Crew(
+            agents=[
+                self.agents.study_planner_agent(), 
+                self.agents.scheduler_agent(), 
+                self.agents.reader_agent(), 
+                self.agents.time_estimation_agent(), 
+                self.agents.specialist_manager()
+            ],
+            tasks=[
+                self.tasks.analyze_text_content_task(), 
+                self.tasks.time_estimation_task(), 
+                self.tasks.study_plan_task(), 
+                self.tasks.manage_final_output_task()
+            ],
+            manager_llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0.6),
+            process=Process.hierarchical,
+            verbose=2,
+            memory=True
+        )
 
     def generate_study_plan(self, available_time_slots, text_path):
-        try:
-            # Initialize the tasks
-            analyze_text_task = self.analyze_text_content_task(text_path)
-            time_estimation_task = self.time_estimation_task(analyze_text_task, available_time_slots)
-            study_plan_task = self.study_plan_task(analyze_text_task, time_estimation_task, available_time_slots)
-            final_output_task = self.manage_final_output_task(study_plan_task, available_time_slots)
+        # Kickoff the Crew AI process
+        result = self.study_plan_crew.kickoff(inputs={"available_time_slots": available_time_slots, "text_path": text_path})
 
-            # Create a crew with the tasks
-            crew = Crew(
-                agents=[self.agents.reader_agent(), self.agents.time_estimation_agent(), self.agents.scheduler_agent(), self.agents.specialist_manager()],
-                tasks=[analyze_text_task, time_estimation_task, study_plan_task, final_output_task],
-                process=Process.sequential
-            )
-
-            # Kick off the crew to execute tasks
-            result = crew.kickoff(inputs={'text_path': text_path, 'available_time_slots': available_time_slots})
-            return result
-        except Exception as e:
-            print(f"Error generating study plan: {e}")
+        # Return study plan summary
+        if result:
+            with open("final_study_plan.md", "r") as f:
+                study_plan_summary = f.read()
+            return study_plan_summary
+        else:
             return None
-
-    def analyze_text_content_task(self, text_path):
-        return Task(
-            description=("Analyze the text content {text_path} to extract key insights for study planning."
-                         "This includes summarizing the content, identifying key topics, and estimating the number of pages."),
-            expected_output=("A summary of key insights from the text content."
-                             "The number of pages in that text and the main topics covered."),
-            tools=[FileReadTool()],
-            agent=self.agents.reader_agent(),
-            async_execution=False,
-            output_file="md_analysis_summary.md"
-        )
-
-    def time_estimation_task(self, analyze_text_task, available_time_slots):
-        return Task(
-            description=("Estimate the time needed to study different topics based on the extracted content."
-                         "And the available time slots {available_time_slots}, and the number of pages in the text."),
-            expected_output="An estimated study time for each topic and suggestions for optimizing study time.",
-            context=[analyze_text_task],
-            agent=self.agents.time_estimation_agent(),
-            output_file="time_estimation.md"
-        )
-
-    def study_plan_task(self, analyze_text_task, time_estimation_task, available_time_slots):
-        return Task(
-            description="Create a personalized study schedule based on the extracted content and available time slots that are {available_time_slots}.",
-            expected_output="A detailed study schedule, study tips, and estimated number of pages to study.",
-            context=[analyze_text_task, time_estimation_task],
-            agent=self.agents.scheduler_agent(),
-            output_file="personalized_study_plan.md"
-        )
-
-    def manage_final_output_task(self, study_plan_task, available_time_slots):
-        return Task(
-            description=("Manage the final output to ensure quality and accuracy."
-                         "We must make sure that the content to be studied is distributed in the time that the user has enabled the {available_time_slots}."),
-            expected_output="A well-structured and detailed study plan that meets the highest standards.",
-            context=[study_plan_task],
-            tools=[SerperDevTool(n_results=3), ScrapeWebsiteTool()],
-            agent=self.agents.specialist_manager(),
-            output_file="final_study_plan.md"
-        )
